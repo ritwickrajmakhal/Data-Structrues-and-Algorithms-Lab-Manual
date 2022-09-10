9bf35e19f0800de1b69e1ca704dc70a35c4d964f
RITWICK RAJ MAKHAL <100060023+ritwickrajmakhal@users.noreply.github.com>
2022-09-11 04:20:32 +0530

    Circular Queue

--- q3.c
+++ q3.c
@@ -1,57 +1,66 @@
-// 3. Write C program to implement a circular queue using array with following functions: 
-// a) insert 
-// b) delete 
+// 3. Write C program to implement a circular queue using array with following functions:
+// a) insert
+// b) delete
 // c) display
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 5
 int front = -1, rear = -1, queue[MAX];
-void insert(){
-    //cheack queue is full or not
-    if((front==rear+1) || (front==0 && rear==MAX-1)){
+void insert()
+{
+    // cheack queue is full or not
+    if ((front == rear + 1) || (front == 0 && rear == MAX - 1))
+    {
         printf("queue is full\n");
         return;
     }
-    else{
-        if(front==-1){
+    else
+    {
+        if (front == -1)
+        {
             front = 0;
         }
-        rear = (rear+1)%MAX;
+        rear = (rear + 1) % MAX;
         printf("Enter a value ");
-        scanf("%d",&queue[rear]);
+        scanf("%d", &queue[rear]);
     }
 }
 void display()
 {
-    //cheack queue is empty or not
-    if(front==-1)
+    // cheack queue is empty or not
+    if (front == -1)
     {
         printf("Queue is empty\n");
         return;
     }
     int i;
-    for(i=front;i!=rear;i=(i+1)%MAX){
-        printf("%d ",queue[i]);
+    for (i = front; i != rear; i = (i + 1) % MAX)
+    {
+        printf("%d ", queue[i]);
     }
-    printf("%d",queue[i]);
+    printf("%d", queue[i]);
     printf("\n");
 }
-void del(){
-    //cheack queue is empty or not
-    if(front==-1){
+void del()
+{
+    // cheack queue is empty or not
+    if (front == -1)
+    {
         printf("Queue is empty\n");
         return;
     }
-    else if(front==rear){ // Atleast 1 element present in a queue
+    printf("Deleted element is %d\n", queue[front]);
+    if (front == rear)
+    { // Atleast 1 element present in a queue
         front = rear = -1;
     }
-    else{
-        int val = queue[front];
-        printf("Deleted element is %d\n",val);
-        front = (front+1)%MAX;
+    else
+    {
+        front = (front + 1) % MAX;
     }
 }
-int main() {
+int main()
+{
     while (1)
     {
         int choice;
@@ -77,5 +86,4 @@ int main() {
         }
     }
     return 0;
-
 }
